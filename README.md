76ca02cd52086157e476c2955ae931aa00431db7
CherryOverBoard <cherry.over.board@gmail.pl>
2023-02-16 18:14:15 +0100

    improvements, license, readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# TIMER
+## Launch
+Simply run `start.cmd`
+
+## What it does?
+From start till close it measures your on-screen time. If you lock screen or screen will lock automatically or computer goes to sleep mode then the application with start to measure off-screen time.
+
+Your on-screen, off-screen and total time from start is shown in the window and pushed into log file every 5 minutes.
+
+Application also pushes times to logs upon close.
+
+## License
+
+Application is licensed under Apache 2.0 License.
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# Copyright 2023 CherryOverBoard
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import datetime
 import tkinter as tk
 import time
@@ -7,20 +21,27 @@ import subprocess
 class Timer:
 
     def __init__(self):
-        self.window = tk.Tk(className="Screen Timer")
+        self.window = tk.Tk()
+        self.window.title("Timer")
+        self.window.geometry("200x100")
+
         self.start_date = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
         self.start_time = time.time_ns()
+
         self.lbl_screen_total = tk.Label()
         self.lbl_screen_total.pack()
         self.lbl_screen_on = tk.Label()
         self.lbl_screen_on.pack()
         self.lbl_screen_off = tk.Label()
         self.lbl_screen_off.pack()
+
         self.screen_on = True
         self.screen_lock_start = time.time_ns()
         self.screen_off_time = 0
-        self.update_counter()
+
+        self.update_timer()
         self.log_time()
+
         self.window.protocol("WM_DELETE_WINDOW", self.on_destroy)
         self.window.mainloop()
 
@@ -35,13 +56,13 @@ class Timer:
                 self.screen_off_time += int((time.time_ns() - self.screen_lock_start) / 1000000000)
             self.screen_on = True
 
-    def update_counter(self):
+    def update_timer(self):
         self.check_screen()
         time_passed = int((time.time_ns() - self.start_time) / 1000000000)
         self.lbl_screen_total.configure(text="Total time: " + self.format_time(time_passed))
         self.lbl_screen_on.configure(text="Time on: " + self.format_time(time_passed - self.screen_off_time))
         self.lbl_screen_off.configure(text="Time off: " + self.format_time(self.screen_off_time))
-        self.window.after(1000, self.update_counter)
+        self.window.after(1000, self.update_timer)
 
     @staticmethod
     def format_time(ttf):
